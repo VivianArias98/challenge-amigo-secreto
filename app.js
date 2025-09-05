728b5b9cdbdecce3871ef2dec8e850f0f71c02aa
VivianArias98 <vivianarias1998@gmail.com>
2025-09-05 11:22:05 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -13,11 +13,22 @@ function agregarAmigo() {
 
     console.log(amigos);
     limpiarCaja();
+    mostrarLista();            
 }
  return;   
 }
 
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
+    
+}
+
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
     
 }
